ee850075dcb7a4d31588a4db08781b8f500dd5b4
Becky Zorn <bczorn@gmail.com>
2019-08-22 04:42:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if array.size >= 3
-    array.join(, and )
+    array.join(", and ")
   if array.size == 2
-    array.join( and )
+    array.join(" and ")
   else
     array.
   end
