8a73a94fb19040142d03a6eed9b7b642c344f5f3
Becky Zorn <bczorn@gmail.com>
2019-08-22 05:01:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   
   if array.size >=3
     last = array.pop
-    array.push(" and #{last}")
+    array.push("and #{last}")
     array.join(", ")
   elsif array.size == 2
     last = array.pop
